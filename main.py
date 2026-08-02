a9b21ff94206115d4719201bd000aefe8ff7658d
Mozib Shaikh <mozib.shiakh06@gmail.com>
2026-08-02 21:37:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def welcome():
+    print("Welcome to Python")
+
+def greet(name):
+    print("Hello", name)
+
+def square():
+    return 5 * 5
+
+def add(a, b):
+    return a + b
+
+welcome()
+greet("Mozib")
+
+result1 = square()
+print("Square =", result1)
+
+result2 = add(10, 20)
+print("Sum =", result2)
