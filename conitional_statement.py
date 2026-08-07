dc76a31b8cbf434a80c75cf804ba0e5fb0e854e9
Mozib Shaikh <mozib.shiakh06@gmail.com>
2026-08-07 20:51:27 +0530

    examples

--- conitional_statement.py
+++ conitional_statement.py
@@ -3,7 +3,7 @@ marks = int(input("Enter your marks: "))
 if marks >= 90:
     print("Grade A")
 elif marks >= 75:
-    print("Grade B")
+    print("Grade B.")
 elif marks >= 50:
     print("Grade C")
 else:
