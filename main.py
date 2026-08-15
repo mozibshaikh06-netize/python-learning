b1ca8ba568b397b85d393e2d33aa0c942663881c
Mozib Shaikh <mozib.shaikh06@gmail.com>
2026-08-15 21:09:40 +0530

    arithemtaic operations

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print("Sum =", result2)
 a = int(input("Enter first number: "))
 b = int(input("Enter second number.: "))
 
-print("Addition =", a + b)
+print("Addition  =", a + b)
 print("Subtraction =", a - b)
 print("Multiplication =", a * b)
 print("Division =", a / b)
